5d6621aa6ccc07646a5502a7cdea51bf11d60604
David Wetzel <david@familycatproductions.com>
2024-01-03 20:52:51 -0600

    navigation bug fix

--- scripts/pageLayout.js
+++ scripts/pageLayout.js
@@ -23,24 +23,9 @@ let sequenceDiv = document.getElementById("sequences");
 let backButton = document.getElementById("back-button");
 let forwardButton = document.getElementById("forward-button");
 let sequenceNum = 0;
-/*
-forwardButton.addEventListener('click', () => {
-  sequenceNum ++;
-  if(sequenceNum > 49){
-    sequenceNum = 49; // stop 4 from the end
-  }
-  let y = sequenceNum * 74.25;
-  sequenceDiv.scroll({top: y, left: 0, behavior: "smooth",});
-  console.log(sequenceNum);
-  for(let i = 0; i < sequenceNum; i++){
-//    sketches[i].noLoop();
-  }
-  if(sequenceNum > 0){
-    sketches[sequenceNum-1].noLoop();
-  }
-});
-*/
+
 forwardButton.addEventListener('click', scroll);
+backButton.addEventListener('click', scroll);
 
 function scroll(e){
   let inc = 0; // increment
@@ -64,35 +49,16 @@ function scroll(e){
   }
   let id = "sequence_" + (sequenceNum +1);
   let seq = document.getElementById(id);
-//  let pos = seq.getRelativePos(seq);
   let y = seq.offsetTop;
   //let y = sequenceNum * 74.25;
-  //sequenceDiv.scroll({top: y, left: 0, behavior: "smooth",});
   sequenceDiv.scroll({top: y, left: 0, behavior: "smooth",});
-//  console.log(sequenceNum);
-  for(let i = sequenceNum + 4; i < 53; i++){
-    sketches[i].noLoop(); // unloop everything outside the view
-  }
   if(sequenceNum > 0){
     sketches[sequenceNum-1].noLoop();
   }
+  sketches[sequenceNum+4].noLoop();
 } 
 
- backButton.addEventListener('click', scroll);
-/*
-backButton.addEventListener('click', () => {
-  sequenceNum --;
-  if(sequenceNum < 0){
-    sequenceNum = 0;
-  }
-  let y = sequenceNum * 74.25;
-  sequenceDiv.scroll({top: y, left: 0, behavior: "smooth",})
-  console.log(sequenceNum);
-  for(let i = sequenceNum + 4; i < 53; i++){
-    sketches[i].noLoop(); //unloop everything past the visible range
-  }
-});
-*/
+/** generate custom QR code leading to the hosted page (wherever it is) */
 let qrLink = document.getElementById("QRLink");
 let qrCode = document.createElement("img");
 let link = "https://chart.googleapis.com/chart?chs=100x100&cht=qr&chl=";
